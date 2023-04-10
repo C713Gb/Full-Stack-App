c0eeb92c8d123da5f8fe03943c2f06dedae08d60
C713Gb <banerjeec713@gmail.com>
2023-04-10 09:57:31 +0530

    update

--- db.js
+++ db.js
@@ -1,13 +1,25 @@
 import mysql from "mysql2";
 
 let db = mysql.createPool({
-  host: "localhost",
+  host: '127.0.0.1',
   user: "root",
   password: "root",
   database: "historical_prices",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
+
+// Attempt to catch disconnects
+db.on("connection", function (connection) {
+  console.log("DB Connection established");
+
+  connection.on("error", function (err) {
+    console.error(new Date(), "MySQL error", err.code);
+  });
+  connection.on("close", function (err) {
+    console.error(new Date(), "MySQL close", err);
+  });
 });
 
 export default db;
